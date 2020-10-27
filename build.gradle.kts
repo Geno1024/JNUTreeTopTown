4a1fceeda51a5ee60e8100d98a3a86edef338f51
Y. Z. Chen <754097987@qq.com>
2020-10-28 01:38:09 +0800

    {INIT} is this the full set?
    Took 14 minutes

---
+++ build.gradle.kts
@@ -0,0 +1,74 @@
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
+
+plugins {
+    kotlin("multiplatform") version "1.4.10"
+    application
+}
+
+group = "com.geno1024"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+    maven {
+        url = uri("https://dl.bintray.com/kotlin/ktor")
+    }
+    maven {
+        url = uri("https://dl.bintray.com/kotlin/kotlinx")
+    }
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "11"
+        }
+        withJava()
+    }
+    js(IR) {
+        browser {
+            binaries.executable()
+            webpackTask {
+                cssSupport.enabled = true
+            }
+            runTask {
+                cssSupport.enabled = true
+            }
+        }
+    }
+    sourceSets {
+        val commonMain by getting
+        val jvmMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-server-netty:1.4.0")
+                implementation("io.ktor:ktor-html-builder:1.4.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
+            }
+        }
+        val jsMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-html:0.7.2")
+            }
+        }
+    }
+}
+
+application {
+    mainClassName = "ServerKt"
+}
+
+tasks.getByName<KotlinWebpack>("jsBrowserProductionWebpack") {
+    outputFileName = "output.js"
+}
+
+tasks.getByName<Jar>("jvmJar") {
+    dependsOn(tasks.getByName("jsBrowserProductionWebpack"))
+    val jsBrowserProductionWebpack = tasks.getByName<KotlinWebpack>("jsBrowserProductionWebpack")
+    from(File(jsBrowserProductionWebpack.destinationDirectory, jsBrowserProductionWebpack.outputFileName))
+}
+
+tasks.getByName<JavaExec>("run") {
+    dependsOn(tasks.getByName<Jar>("jvmJar"))
+    classpath(tasks.getByName<Jar>("jvmJar"))
+}
